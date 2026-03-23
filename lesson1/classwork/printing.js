2ff218dd9f81d250ea3f7e0a6bdbe720dcc0cd5b
declan-bit <declanshurtz@gmail.com>
2026-03-23 00:57:55 +0000

    finished lesson one

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+console.log("my name is Declan")
+console.log('my favroite food is pizza')
+//console.log ('this dosent work")'))
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,7 @@
+let x = 5;  // number: whole-number value
+console.log(x)
+
+let number = 3.14  //  number: decimal number
+console.log(number)
+
+
